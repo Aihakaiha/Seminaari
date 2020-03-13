e1ef4b65d30fa0f34574fa1c1e02829daa879187
KloppRanar <59951688+KloppRanar@users.noreply.github.com>
2020-03-13 10:06:31 +0200

    Add files via upload

--- adminregisteri.php
+++ adminregisteri.php
@@ -1,23 +1,23 @@
 ﻿<?php
 
-$enimi = $_GET["etunimi"];
-$snimi = $_GET["sukunimi"];
-$losoite = $_GET["osoite"];
-$postinro = $_GET["postinumero"];
-$postitp = $_GET["postitoimipaikka"];
-$puh = $_GET["puhelinnumero"];
-$email = $_GET["sahkoposti"];
-$org = $_GET["tyonantaja/oppilaitos"];
-$ammatti = $_GET["ammattinimike"];
-$bd = $_GET["syntymaaika"];
-$hinta = $_GET["hintavaihtoehdot"];
-$erv = $_GET["erikosruokavalio"];
-$lisainfo = $_GET["lisatietoja"];
+$enimi = $_GET["enimi"];
+$snimi = $_GET["snimi"];
+$losoite = $_GET["losoite"];
+$postinro = $_GET["postinro"];
+$postitp = $_GET["postitp"];
+$puh = $_GET["puh"];
+$email = $_GET["email"];
+$org = $_GET["org"];
+$ammatti = $_GET["ammatti"];
+$bd = $_GET["bd"];
+$hinta = $_GET["hinta"];
+$erv = $_GET["erv"];
+$lisainfo = $_GET["lisainfo"];
 
 $palvelin = "localhost";
 $kayttaja = "root";
 $salasana = "";
-tietokanta = "seminari";
+$tietokanta = "seminaari";
 
 $link = mysqli_connect($palvelin, $kayttaja, $salasana, $tietokanta);
 
@@ -26,12 +26,12 @@ if(mysqli_connect_error())
 	die("Tietokantaan ei saatu yhteyttä");
 }
 
-$inquiry = "INSERT INTO seminarirekisteri (etunimi, sukunimi, osoite, postinumero, postitoimipaikka, puhelinnumero, sahkoposti, tyonantaja, ammattinimike, 
+$inquiry = "INSERT INTO adminregisteri (etunimi, sukunimi, osoite, postinumero, postitoimipaikka, puhelinnumero, sahkoposti, tyonantaja, ammattinimike, 
 syntymaaika, hintavaihtoehdot, erikosruokavalio, lisatietoja) VALUES
 ('$enimi', '$snimi', '$losoite', '$postinro', '$postitp', '$puh', '$email', '$org', '$ammatti', '$bd', '$hinta', '$erv', '$lisainfo')";
 if (mysqli_query($link, $inquiry))
 {
-	echo "Sinun tietosi on tallennettu"
+	echo "Sinun tietosi on tallennettu";
 }
 else 
 {
